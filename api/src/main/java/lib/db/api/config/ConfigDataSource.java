d87d0afbb8a3da96f9c00789ffdc0f94070bff5f
John <john.l.bidlack@gmail.com>
2024-12-08 12:39:33 -0500

    laptop changes. May be depreciated

--- api/src/main/java/lib/db/api/config/ConfigDataSource.java
+++ api/src/main/java/lib/db/api/config/ConfigDataSource.java
@@ -4,28 +4,18 @@ import java.beans.PropertyVetoException;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 
 @Configuration
 public class ConfigDataSource {
 
-    @Value("${spring.datasource.url}")
-    private String url;
-
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverClassName;
+    private String url = Variables.getUrl();
+    private String username = Variables.getUsername();
+    private String password = Variables.getPassword();
+    private String driverClassName ;
 
     @Bean
     public DataSource dataSource() throws PropertyVetoException{
--- api/src/main/java/lib/db/api/config/Variables.java
+++ api/src/main/java/lib/db/api/config/Variables.java
@@ -1,26 +1,55 @@
 package lib.db.api.config;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Variables {
+    private static String url = "";
+    private static String username = "";
+    private static String password = "";
+    private static String driver = "";
+    private static String KEY = "";
+    private static String VITE_KEY = "";
 
-    
-    @Autowired
-    private Environment env;
+    public Variables(){
+        try(FileInputStream fis = new FileInputStream("api\\src\\main\\resources\\application.properties")){
+        Properties prop = new Properties();
+        
+        url = prop.getProperty("db.url");
+        username = prop.getProperty( "db.url");
+        password = prop.getProperty("db.password");
+        driver = prop.getProperty("db.driver");
+        KEY = prop.getProperty("db.KEY");
+        VITE_KEY = prop.getProperty("db.VITE_KEY");
 
-    private String url = env.getProperty("spring.datasource.url");
-    private String username = env.getProperty("spring.datasource.username");
-    private String password = env.getProperty("spring.datasource.password");
-    private String driver = env.getProperty("spring.datasource.driver-class-name");
+        }
+        catch(FileNotFoundException fne){
+            fne.printStackTrace();
+        }
+        catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+
+    }
+
+    // private String url = env.getProperty("spring.datasource.url");
+    // private String username = env.getProperty("spring.datasource.username");
+    // private String password = env.getProperty("spring.datasource.password");
+    // private String driver = env.getProperty("spring.datasource.driver-class-name");
     // private final static String url = "jdbc:postgresql://aws-0-us-west-1.pooler.supabase.com:6543/postgres";
     // private final static String username = "postgres.nmkipvazyvwaegxfqotg";
     // private final static String password = "LjWIbmC62AG3Nfdz";
     // private final static String driver = "org.postgresql.Driver";
-    private final static String KEY = "Ir3allyWantthisPr0ject2BDoneS00n";
-    private final static String VITE_KEY = "WhyAm1CreatingIt";
+    // private final static String KEY = "Ir3allyWantthisPr0ject2BDoneS00n";
+    // private final static String VITE_KEY = "WhyAm1CreatingIt";
 
     public static String getViteKey() {
         return VITE_KEY;
@@ -28,16 +57,16 @@ public class Variables {
     public static String getKey() {
         return KEY;
     }
-    public String getUrl() {
+    public static String getUrl() {
         return url;
     }
-    public String getUsername() {
+    public static String getUsername() {
         return username;
     }
-    public String getPassword() {
+    public static String getPassword() {
         return password;
     }
-    public String getDriver() {
+    public static String getDriver() {
         return driver;
     }
 
