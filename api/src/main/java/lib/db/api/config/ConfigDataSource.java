c651e0155f746fa1f20f9c2e0e8d11c0b880a434
JBidlack <john.l.bidlack@gmail.com>
2025-01-02 10:50:17 -0500

    pushing changes made pre-hoilday. Not sure exactly what I changed, but it seems to resolve some f the issues I had when trying to run at home.

--- api/src/main/java/lib/db/api/config/ConfigDataSource.java
+++ api/src/main/java/lib/db/api/config/ConfigDataSource.java
@@ -1,43 +1,30 @@
 package lib.db.api.config;
 
-import java.beans.PropertyVetoException;
-
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-
 
 @Configuration
 public class ConfigDataSource {
 
-    @Value("${spring.datasource.url}")
-    private String url;
+    private final Variables v;
 
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverClassName;
+    @Autowired
+    public ConfigDataSource(Variables v) {
+        this.v = v;
+    }
 
     @Bean
-    public DataSource dataSource() throws PropertyVetoException{
-
+    public DataSource dataSource() {
         DataSourceBuilder<?> dsb = DataSourceBuilder.create();
 
-        dsb.driverClassName(driverClassName);
-        dsb.url(url);
-        dsb.username(username);
-        dsb.password(password);
+        dsb.driverClassName(v.getDriver());
+        dsb.url(v.getUrl());
+        dsb.username(v.getUsername());
+        dsb.password(v.getPassword());
 
         return dsb.build();
     }
-
 }
--- api/src/main/java/lib/db/api/config/Variables.java
+++ api/src/main/java/lib/db/api/config/Variables.java
@@ -1,44 +1,59 @@
 package lib.db.api.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Component
 public class Variables {
 
-    
-    @Autowired
-    private Environment env;
+    private String url;
+    private String username;
+    private String password;
+    private String driver;
 
-    private String url = env.getProperty("spring.datasource.url");
-    private String username = env.getProperty("spring.datasource.username");
-    private String password = env.getProperty("spring.datasource.password");
-    private String driver = env.getProperty("spring.datasource.driver-class-name");
-    // private final static String url = "jdbc:postgresql://aws-0-us-west-1.pooler.supabase.com:6543/postgres";
-    // private final static String username = "postgres.nmkipvazyvwaegxfqotg";
-    // private final static String password = "LjWIbmC62AG3Nfdz";
-    // private final static String driver = "org.postgresql.Driver";
     private final static String KEY = "Ir3allyWantthisPr0ject2BDoneS00n";
     private final static String VITE_KEY = "WhyAm1CreatingIt";
+
+    public Variables() {
+        Properties prop = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                throw new IOException("application.properties not found in classpath.");
+            }
+            prop.load(input);
+            url = prop.getProperty("spring.datasource.url");
+            username = prop.getProperty("spring.datasource.username");
+            password = prop.getProperty("spring.datasource.password");
+            driver = prop.getProperty("spring.datasource.driver-class-name");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
 
     public static String getViteKey() {
         return VITE_KEY;
     }
+
     public static String getKey() {
         return KEY;
     }
+
     public String getUrl() {
         return url;
     }
+
     public String getUsername() {
         return username;
     }
+
     public String getPassword() {
         return password;
     }
+
     public String getDriver() {
         return driver;
     }
-
 }
